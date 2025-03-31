0867e0a465a35edee5b5b47e8a63434201fc4a87
Sachin Shah <sachin_shah@redmane.com>
2025-03-31 17:35:51 -0500

    Added more Read and Transform methods

--- DataExtractAndImport/Export.cs
+++ DataExtractAndImport/Export.cs
@@ -1,33 +1,19 @@
 using ClosedXML.Excel;
-using DocumentFormat.OpenXml.Office2013.Excel;
-using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
-using DocumentFormat.OpenXml.Presentation;
 using Microsoft.Data.SqlClient;
 
 namespace DataExtractAndImport;
 
-internal class SecurityRole
+internal class SecurityRole(int securityRoleId, string name)
 {
-    public int SecurityRoleId { get; }
-    public string Name { get; set; }
-
-    public SecurityRole(int securityRoleId, string name)
-    {
-        SecurityRoleId = securityRoleId;
-        Name = name;
-    }
+    public int SecurityRoleId { get; } = securityRoleId;
+    public string Name { get; set; } = name;
 
     public override bool Equals(object? obj) => Equals(obj as SecurityRole);
 
-    private bool Equals(SecurityRole? obj) => obj != null && SecurityRoleId == obj.SecurityRoleId;
+    public bool Equals(SecurityRole? obj) => obj != null && SecurityRoleId == obj.SecurityRoleId;
 
     public override int GetHashCode() => SecurityRoleId;
 
-    /// <summary>
-    /// Reads the security roles from the mCase database.
-    /// </summary>
-    /// <param name="conn">Connection to SQL database</param>
-    /// <returns>List of roles from the database.</returns>
     public static List<SecurityRole> Read(SqlConnection conn)
     {
         Console.WriteLine("Started reading security roles.");
@@ -43,28 +29,34 @@ internal class SecurityRole
         return ret;
     }
 
-    public static List<SecurityRole> Transform(List<SecurityRole> roles)
-    {
-        roles.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.OrdinalIgnoreCase));
-        return roles;
-    }
+    public static List<SecurityRole> Transform(List<SecurityRole> roles) => [.. roles.OrderBy(x => x.Name.ToUpper())];
 
-    public static bool Write()
-    {
-        throw new NotImplementedException();
-    }
+    public static bool Write() => throw new NotImplementedException();
 }
 
-internal class WorkQueue
+internal class WorkQueue(int workQueueId, string name)
 {
-    public int WorkQueueId { get; set; }
-    public string Name { get; set; }
+    public int WorkQueueId { get; set; } = workQueueId;
+    public string Name { get; set; } = name;
 
-    public WorkQueue(int workQueueId, string name)
+    public static List<WorkQueue> Read(SqlConnection conn)
     {
-        WorkQueueId = workQueueId;
-        Name = name;
+        Console.WriteLine("Started reading work queues.");
+        const string sql = "select WorkqueueID, Name from WorkQueue";
+        using var cmd = new SqlCommand(sql, conn);
+        var rdr = cmd.ExecuteReader();
+        var ret = new List<WorkQueue>();
+
+        while (rdr.Read()) ret.Add(new WorkQueue(rdr.GetInt32(0), rdr.GetString(1)));
+
+        rdr.Close();
+        Console.WriteLine("Finished reading work queues.");
+        return ret;
     }
+
+    public static List<WorkQueue> Transform(List<WorkQueue> q) => [.. q.OrderBy(x => x.Name.ToUpper())];
+
+    public static bool Write() => throw new NotImplementedException();
 }
 
 internal enum SubscriptionType
@@ -80,74 +72,181 @@ internal class WorkQueueSubscription
     public SecurityRole? SecurityRole { get; set; }
     public SubscriptionType SubscriptionType { get; set; }
 
-    public WorkQueueSubscription(WorkQueue workQueue, User? user, SubscriptionType subscriptionType)
+    public WorkQueueSubscription(WorkQueue workQueue, User? user)
     {
         WorkQueue = workQueue;
         User = user;
-        SubscriptionType = subscriptionType;
+        SecurityRole = null;
+        SubscriptionType = SubscriptionType.User;
     }
 
-    public WorkQueueSubscription(WorkQueue workQueue, SecurityRole? securityRole, SubscriptionType subscriptionType)
+    public WorkQueueSubscription(WorkQueue workQueue, SecurityRole? securityRole)
     {
         WorkQueue = workQueue;
+        User = null;
         SecurityRole = securityRole;
-        SubscriptionType = subscriptionType;
+        SubscriptionType = SubscriptionType.Role;
     }
+
+    public static List<WorkQueueSubscription> Read(SqlConnection conn, List<WorkQueue> queues, List<User> users,
+        List<SecurityRole> roles)
+    {
+        Console.WriteLine("Started reading queue subscriptions - user.");
+        var sql = "select WorkQueueID, UserID from WorkQueueMember";
+        using var cmd = new SqlCommand(sql, conn);
+        var rdr = cmd.ExecuteReader();
+        var ret = new List<WorkQueueSubscription>();
+
+        while (rdr.Read())
+        {
+            var q = queues.FirstOrDefault(x => x.WorkQueueId == rdr.GetInt32(0));
+            var u = users.FirstOrDefault(x => x.UserId == rdr.GetInt32(1));
+            if (u == null || q == null)
+            {
+                Console.WriteLine($"Unable to find queue {rdr.GetInt32(0)} or user {rdr.GetInt32(1)}");
+                continue;
+            }
+
+            ret.Add(new WorkQueueSubscription(q, u));
+        }
+
+        rdr.Close();
+        Console.WriteLine("Finished reading queue subscriptions - user.");
+
+        Console.WriteLine("Started reading queue subscriptions - role.");
+        sql = "select WorkQueueID, RoleID from WorkQueueRole";
+        using var cmd2 = new SqlCommand(sql, conn);
+        rdr = cmd2.ExecuteReader();
+
+        while (rdr.Read())
+        {
+            var q = queues.FirstOrDefault(x => x.WorkQueueId == rdr.GetInt32(0));
+            var r = roles.FirstOrDefault(x => x.SecurityRoleId == rdr.GetInt32(1));
+            if (r == null || q == null)
+            {
+                Console.WriteLine($"Unable to find queue {rdr.GetInt32(0)} or role {rdr.GetInt32(1)}");
+                continue;
+            }
+
+            ret.Add(new WorkQueueSubscription(q, r));
+        }
+
+        rdr.Close();
+        Console.WriteLine("Finished reading queue subscriptions - role.");
+        return ret;
+    }
+
+    public static List<WorkQueueSubscription> Transform(List<WorkQueueSubscription> wqs) => wqs;
+    public static void Write() => throw new NotImplementedException();
 }
 
-internal class User
+internal class User(
+    int userId,
+    string userName,
+    string firstName,
+    string lastName,
+    string emailAddress,
+    string phone,
+    bool externalAuthentication,
+    int active,
+    int supervisorId)
 {
-    public int UserId { get; set; }
-    public string UserName { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string EmailAddress { get; set; }
-    public string Phone { get; set; }
-    public bool ExternalAuthentication { get; set; }
-    public int Active { get; set; }
-    public int Supervisor { get; set; }
+    public int UserId { get; set; } = userId;
+    public string UserName { get; set; } = userName;
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public string EmailAddress { get; set; } = emailAddress;
+    public string Phone { get; set; } = phone;
+    public bool ExternalAuthentication { get; set; } = externalAuthentication;
+    public int Active { get; set; } = active;
+    public int SupervisorId { get; set; } = supervisorId;
+    public User? Supervisor { get; set; } = null;
 
-    public User(int userId, string userName, string firstName, string lastName, string email, string emailAddress,
-        string phone, bool externalAuthentication, int active, int supervisor)
+    public static List<User> Read(SqlConnection conn)
     {
-        UserId = userId;
-        UserName = userName;
-        FirstName = firstName;
-        LastName = lastName;
-        Email = email;
-        EmailAddress = emailAddress;
-        Phone = phone;
-        ExternalAuthentication = externalAuthentication;
-        Active = active;
-        Supervisor = supervisor;
+        Console.WriteLine("Started reading users.");
+        const string sql =
+            "select UserID, Username, FirstName, LastName, Email, MobilePhoneNumber, ExternalAuthorization, Status, Supervisor from users";
+        using var cmd = new SqlCommand(sql, conn);
+        var rdr = cmd.ExecuteReader();
+        var ret = new List<User>();
+
+        while (rdr.Read())
+            ret.Add(new User(rdr.GetInt32(0), rdr.IsDBNull(1) ? "" : rdr.GetString(1),
+                rdr.IsDBNull(2) ? "" : rdr.GetString(2),
+                rdr.IsDBNull(3) ? "" : rdr.GetString(3), rdr.IsDBNull(4) ? "" : rdr.GetString(4),
+                rdr.IsDBNull(5) ? "" : rdr.GetString(5), rdr.GetBoolean(6), rdr.GetInt32(7),
+                rdr.IsDBNull(8) ? -1 : rdr.GetInt32(8)));
+
+        rdr.Close();
+
+        foreach (var u in ret)
+            u.Supervisor = u.SupervisorId == -1 ? u : ret.FirstOrDefault((x => x.UserId == u.SupervisorId), u);
+
+        Console.WriteLine("Finished reading users.");
+        return ret;
     }
+
+    public static List<User> Transform(List<User> users) =>
+        users.OrderBy(x => $"{x.FirstName.ToUpper()} {x.LastName.ToUpper()} {x.UserName.ToUpper()}").ToList();
+
+    public static void Write() => throw new NotImplementedException();
 }
 
-internal class Datalist
+internal class Datalist(int datalistId, string name, string systemName)
 {
-    public int DatalistId { get; set; }
-    public string Name { get; set; }
-    public string SystemName { get; set; }
+    public int DatalistId { get; set; } = datalistId;
+    public string Name { get; set; } = name;
+    public string SystemName { get; set; } = systemName;
 
-    public Datalist(int datalistId, string name, string systemName)
+    public static List<Datalist> Read(SqlConnection conn)
     {
-        DatalistId = datalistId;
-        Name = name;
-        SystemName = systemName;
+        Console.WriteLine("Started reading datalists.");
+        const string sql = "select DataListID, Name, SystemName from DataList";
+        using var cmd = new SqlCommand(sql, conn);
+        var rdr = cmd.ExecuteReader();
+        var ret = new List<Datalist>();
+
+        while (rdr.Read())
+            ret.Add(new Datalist(rdr.GetInt32(0), rdr.IsDBNull(1) ? "" : rdr.GetString(1),
+                rdr.IsDBNull(2) ? "" : rdr.GetString(2)));
+
+        rdr.Close();
+        Console.WriteLine("Finished reading datalists.");
+        return ret;
     }
+
+    public static List<Datalist> Transform(List<Datalist> dls) => dls.OrderBy(x => x.Name.ToUpper()).ToList();
+    public static void Write() => throw new NotImplementedException();
 }
 
-internal class ListRelationship
+internal class ListRelationship(Datalist parent, Datalist child)
 {
-    public Datalist Parent { get; set; }
-    public Datalist Child { get; set; }
+    public Datalist Parent { get; set; } = parent;
+    public Datalist Child { get; set; } = child;
 
-    public ListRelationship(Datalist parent, Datalist child)
+    public static List<ListRelationship> Read(SqlConnection conn, List<Datalist> dls)
     {
-        Parent = parent;
-        Child = child;
+        Console.WriteLine("Started reading list relationships.");
+        const string sql = "select ParentListID, ChildListID from ListRelationship";
+        using var cmd = new SqlCommand(sql, conn);
+        var rdr = cmd.ExecuteReader();
+        var ret = new List<ListRelationship>();
+
+        while (rdr.Read())
+        {
+            var p = dls.First(x => x.DatalistId == rdr.GetInt32(0));
+            var c = dls.First(x => x.DatalistId == rdr.GetInt32(1));
+            ret.Add(new ListRelationship(p, c));
+        }
+
+        rdr.Close();
+        Console.WriteLine("Finished reading list relationships.");
+        return ret;
     }
+
+    public static List<ListRelationship> Transform(List<ListRelationship> lrs) => lrs;
+    public static void Write() => throw new NotImplementedException();
 }
 
 internal class Permission
@@ -264,6 +363,10 @@ internal class Permission
                (Delete ? ", Delete" : "") + (ViewActivity ? ", Activity Wall" : "") + (Merge ? ", Merge" : "") +
                (Move ? ", Move" : "");
     }
+
+    public static void Read() => throw new NotImplementedException();
+    public static void Transform() => throw new NotImplementedException();
+    public static void Write() => throw new NotImplementedException();
 }
 
 internal class ListRole
@@ -278,6 +381,10 @@ internal class ListRole
         SecurityRole = securityRole;
         Permission = permission;
     }
+
+    public static void Read() => throw new NotImplementedException();
+    public static void Transform() => throw new NotImplementedException();
+    public static void Write() => throw new NotImplementedException();
 }
 
 internal class Export
@@ -292,17 +399,19 @@ internal class Export
     private string workQueueMatrixSheetName = "WorkQueueMatrix";
 
 
-    public void Run(String ConnectionString, string Filename)
+    public void Run(String connectionString, string filename)
     {
         // Excel workbook
         var wb = new XLWorkbook();
 
         // Open a DB connection
-        var conn = new SqlConnection(ConnectionString);
+        var conn = new SqlConnection(connectionString);
         conn.Open();
 
+        // Get and read the data
 
-        Console.WriteLine(ConnectionString);
-        Console.WriteLine(Filename);
+
+        Console.WriteLine(connectionString);
+        Console.WriteLine(filename);
     }
 }
--- DataExtractAndImport/Program.cs
+++ DataExtractAndImport/Program.cs
@@ -10,11 +10,11 @@ internal class ExportOptions
             "A connection string to connect to the mCase database, can rely on interactive authentication or MFA.",
         Default =
             "data source=localhost; initial catalog=mCASE_ADMIN; integrated security=True; min pool size=10; max pool size=500; multipleactiveresultsets=True; application name=mCaseAdminWebEF6; Connect Timeout=1200; Command Timeout=1200;  TrustServerCertificate=True")]
-    public string ConnectionString { get; set; }
+    public string? ConnectionString { get; set; }
 
     [Option('f', "filename", HelpText = "Name of the export file.",
         Default = "SecurityMatrix.xlsx")]
-    public string Filename { get; set; }
+    public string? Filename { get; set; }
 }
 
 [Verb("import", aliases: ["i"], HelpText = "Import data from an Excel file to an mCase instance.")]
@@ -22,7 +22,7 @@ internal class ImportOptions
 {
     [Option('f', "filename", Required = false, HelpText = "Name of the import file.",
         Default = "SecurityMatrix.xlsx")]
-    public string Filename { get; set; }
+    public string? Filename { get; set; }
 }
 
 internal class Program
